74ff22a6db7ed0d317709cb96aae5c229eb07265
Andrey <cdandrey@gmail.com>
2019-09-23 02:10:28 +0300

    Replace myself templates on std templates

--- lib/print_ip.h
+++ lib/print_ip.h
@@ -14,19 +14,7 @@
 namespace ipp
 {
     template<typename T>
-    //not supported gcc 5.4
     std::enable_if_t<std::is_arithmetic<T>::value,std::string>
-    //std::enable_if_t<std::is_same<T,char>::value || 
-    //                 std::is_same<T,unsigned char>::value ||
-    //                 std::is_same<T,short>::value ||
-    //                 std::is_same<T,unsigned short>::value ||
-    //                 std::is_same<T,int>::value ||
-    //                 std::is_same<T,unsigned int>::value ||
-    //                 std::is_same<T,long int>::value ||
-    //                 std::is_same<T,long unsigned int>::value ||
-    //                 std::is_same<T,long long int>::value ||
-    //                 std::is_same<T,long long unsigned int>::value,
-    //                 std::string>
     tostr(const T &ip)
     {
         using UT = typename std::make_unsigned<T>::type;
@@ -92,17 +80,10 @@ namespace ipp
     }
     //-------------------------------------
 
-    template<class...> struct conjunction : std::true_type { };
-    template<class B1> struct conjunction<B1> : B1 { };
-    template<class B1, class... Bn>
-    struct conjunction<B1, Bn...> : std::conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
-    
     template<typename T,typename... A>
     struct is_same_tuple 
     { 
-        //not supported gcc 5.4
-        //static constexpr bool value = std::conjunction<std::is_same<T,A>...>::value;
-        static constexpr bool value = conjunction<std::is_same<T,A>...>::value;
+        static constexpr bool value = std::conjunction<std::is_same<T,A>...>::value;
     };
 
     template<typename... A>
